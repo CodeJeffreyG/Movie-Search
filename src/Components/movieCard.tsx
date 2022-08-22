1c262c662002d1e2bf8a0328e1ba9cbc4f3cca4a
Jeffrey <jeffrey.graham600@gmail.com>
2022-08-22 15:09:37 -0400

    Refactored code to look nicer

--- src/Components/movieCard.tsx
+++ src/Components/movieCard.tsx
@@ -1,21 +1,23 @@
 export default function MovieCard(props : any) {
+    const {id, title, poster_path, release_date, vote_average, overview} = props
+
   return (
     <div className="card">
       <img
-        key={props.id}
+        key={id}
         className="card--image"
-        src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${props.poster_path}`}
-        alt={`${props.title} poster`}
+        src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${poster_path}`}
+        alt={`${title} poster`}
       />
       <div className="card--content">
-        <h3 className="card--title">{props.title}</h3>
+        <h3 className="card--title">{title}</h3>
         <p>
-          <small>RELEASE DATE: {props.release_date}</small>
+          <small>RELEASE DATE: {release_date}</small>
         </p>
         <p>
-          <small>RATING: {props.vote_average}</small>
+          <small>RATING: {vote_average}</small>
         </p>
-        <p className="card--description">{props.overview}</p>
+        <p className="card--description">{overview}</p>
       </div>
     </div>
   );
