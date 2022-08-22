72e843417fd2da0fb8ebbcd1205878b987de535d
Jeffrey <jeffrey.graham600@gmail.com>
2022-08-22 12:42:44 -0400

    Created asynch function that queries API

--- src/Components/SearchMovie.tsx
+++ src/Components/SearchMovie.tsx
@@ -1,6 +1,27 @@
+import React, { useState } from "react";
+
 export default function MovieSearch() {
+    const [query, setQuery] = useState('');
+
+
+  const searchMovies = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const query = "Jurassic";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
         Movie Name
       </label>
@@ -8,7 +29,7 @@ export default function MovieSearch() {
         className="input"
         type="text"
         name="query"
-        placeholder="i.e Jurassic Parl"
+        placeholder="i.e. Avengers, Incredibles, Ect"
       />
       <button className="button" type="submit">
         Search
