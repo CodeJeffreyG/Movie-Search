81b1ec76e449bd7b8123fd688306ef3ca17a3c5a
Jeffrey <jeffrey.graham600@gmail.com>
2022-08-22 12:46:42 -0400

    useState hook to set whats typ into input as State

--- src/Components/SearchMovie.tsx
+++ src/Components/SearchMovie.tsx
@@ -7,8 +7,6 @@ export default function MovieSearch() {
   const searchMovies = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const query = "Jurassic";
-
     const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
@@ -30,6 +28,8 @@ export default function MovieSearch() {
         type="text"
         name="query"
         placeholder="i.e. Avengers, Incredibles, Ect"
+        value = {query}
+        onChange ={(e) => setQuery(e.target.value)}
       />
       <button className="button" type="submit">
         Search
