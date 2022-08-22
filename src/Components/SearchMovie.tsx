703274f86ccd49687e0c8bbb75b87d240c2b3c84
Jeffrey <jeffrey.graham600@gmail.com>
2022-08-22 14:34:31 -0400

    Added Logic To Card That Renders Movie Poster

--- src/Components/SearchMovie.tsx
+++ src/Components/SearchMovie.tsx
@@ -8,6 +8,8 @@ export default function MovieSearch() {
   const searchMovies = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    if (query.replace(/\s/g, "") === "") return;
+
     const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
@@ -38,8 +40,18 @@ export default function MovieSearch() {
           Search
         </button>
       </form>
-      <div className = "card-list">
-            {movies.map(movie => )}
+      
+      <div className="card-list">
+        {movies.map((movie: any) => (
+          <div className="card">
+            <img
+              key={movie.id}
+              className="card--image"
+              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+              alt={`${movie.title} poster`}
+            />
+          </div>
+        ))}
       </div>
     </>
   );
